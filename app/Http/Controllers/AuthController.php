6ff6baa842a063e47b2e9a76a0839b4cc9fafea5
Jim Lao <laojimmarklino@gmail.com>
2023-06-29 14:37:05 +0800

    Logout logic
    Added Logout Logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,6 +45,10 @@ class AuthController extends Controller
     }
 
     public function logout() {
-        return response()->json('this is my logout method');
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => "You have successfully logout"
+        ]);
     }
 }
